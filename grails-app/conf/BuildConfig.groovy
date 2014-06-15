69031c13b6443476fbe8b1989a0479b1f88ec0b0
Goran Ehrsson <goran@technipelago.se>
2014-06-15 18:13:17 +0200

    Upgraded project to Grails 2.2.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,39 +1,29 @@
+grails.project.work.dir = "target"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits("global") { }
+    log "warn"
     repositories {        
         grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
         mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-        compile("org.apache.tika:tika-parsers:1.1") { exclude "xmlbeans" }
+        compile("org.apache.tika:tika-parsers:1.5") {
+            exclude "xmlbeans"
+            exclude "asm-debug-all"
+        }
 	compile 'org.compass-project:compass:2.2.0'
     }
     plugins {
         build(":tomcat:$grailsVersion",
-              ":rest-client-builder:1.0.2",
-              ":release:2.0.4") {
+                ":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
             export = false
         }
-        runtime(":hibernate:$grailsVersion") {
-            export = false
-        }
+        runtime(":hibernate:$grailsVersion") { export = false }
     }
 }
