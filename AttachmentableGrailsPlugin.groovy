c06e50d07a8a7fd6f19e106279e149525c0ac08f
Goran Ehrsson <goran@technipelago.se>
2014-06-15 18:14:22 +0200

    0.4.0-SNAPSHOT

--- AttachmentableGrailsPlugin.groovy
+++ AttachmentableGrailsPlugin.groovy
@@ -32,7 +32,7 @@ class AttachmentableGrailsPlugin {
 
     static LOG = LogFactory.getLog('com.macrobit.grails.plugins.AttachmentableGrailsPlugin')
 
-    def version = '0.3.0'
+    def version = '0.4.0-SNAPSHOT'
     def grailsVersion = '1.1 > *'
     def dependsOn = [hibernate: '1.1 > *']
     def pluginExcludes = [
